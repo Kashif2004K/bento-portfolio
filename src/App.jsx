f26fa36519975a24b702f8d25210bc9c032ebf8b
kashif <blackwatch4321@gmail.com>
2025-12-29 17:07:46 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,45 @@
 import personImg from "./assets/person.png";
 import sofaImg from "./assets/sofa.png";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <header>
-        <h3>JULIA HUANG</h3>
+    <div className="bento">
+      <header className="header">
+        <p>
+          <span>JULIA</span> HUANG
+        </p>
         <ul>
           <li>PROJECTS</li>
           <li>ABOUT</li>
           <li>CONTACT</li>
         </ul>
       </header>
-      <div>
+      <div className="content">
         <h1>Artist Redefining Architecture with AI-Driven Design</h1>
       </div>
-      <div>
+      <div className="image-container">
         <img src={personImg} alt="person" />
       </div>
-      <div>
+      <div className="projects">
         <p>Musea</p>
         <img src={sofaImg} alt="sofa" />
         <p>Elara</p>
         <p>Verve</p>
         <p>Zephyr</p>
       </div>
-      <div>
+      <div className="about">
         <p>
           Julia Huang is an innovative AI artist, renowned for blending
           cutting-edge technology with creative expression. Based in LA, she
           crafts unique digital art experiences accessible globally.
         </p>
       </div>
-      <div>
+      <div className="contact">
         <p>Have some questions?</p>
         <h2>Contact me</h2>
       </div>
-      <footer>
+      <footer className="footer">
         <ul>
           <li>INSTAGRAM</li>
           <li>TWITTER</li>
