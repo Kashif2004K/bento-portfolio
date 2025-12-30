2cd9b0b30d537ec0452f355572e15e0259dbed76
kashif <blackwatch4321@gmail.com>
2025-12-30 11:50:46 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import personImg from "./assets/person.png";
 import sofaImg from "./assets/sofa.png";
+import arrowImg from "./assets/arrow.svg";
+import circleImg from "./assets/circle.svg";
+import flowerImg from "./assets/flower.svg";
 import "./App.css";
 
 const App = () => {
@@ -16,6 +19,7 @@ const App = () => {
         </ul>
       </header>
       <div className="card content">
+        <img src={flowerImg} alt="flower" />
         <h1>
           Artist Redefining <span>Architecture</span> with AI-Driven Design
         </h1>
@@ -24,8 +28,11 @@ const App = () => {
         <img src={personImg} alt="person" />
       </div>
       <div className="card projects">
-        <p>Musea</p>
-        <img src={sofaImg} alt="sofa" />
+        <div className="arrow-div1">
+          <p>Musea</p>
+          <img src={arrowImg} alt="arrow" />
+        </div>
+        <img className="sofa" src={sofaImg} alt="sofa" />
         <hr />
         <p>Elara</p>
         <hr />
@@ -34,6 +41,7 @@ const App = () => {
         <p>Zephyr</p>
       </div>
       <div className="card about">
+        <img src={circleImg} alt="circle" />
         <p>
           Julia Huang is an innovative AI artist, renowned for blending
           cutting-edge technology with creative expression. Based in LA, she
@@ -41,7 +49,10 @@ const App = () => {
         </p>
       </div>
       <div className="card contact">
-        <p>Have some questions?</p>
+        <div>
+          <p>Have some questions?</p>
+          <img src={arrowImg} alt="arrow" />
+        </div>
         <h2>Contact me</h2>
       </div>
       <footer className="card footer">
